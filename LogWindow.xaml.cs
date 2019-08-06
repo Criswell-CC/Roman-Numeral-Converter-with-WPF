b955f4082aef0d04a9cce30dd308e2e8ec299b51
Criswell-CC <53798287+Criswell-CC@users.noreply.github.com>
2019-08-06 14:43:57 -0500

    Add files via upload

---
+++ LogWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using Roman.Models;
+
+namespace Roman.Windows
+{
+    /// <summary>
+    /// Interaction logic for LogWindow.xaml
+    /// </summary>
+    public partial class LogWindow : Window
+    {
+        public LogWindow()
+        {
+            InitializeComponent();
+            DataContext = new Roman.Models.LogModel();
+        }
+
+        public void LogWindow_Closing(object sender, CancelEventArgs e)
+        {
+            LogModel.windowOpen = false;
+        }
+    }
+}
