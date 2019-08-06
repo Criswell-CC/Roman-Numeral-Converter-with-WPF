5f25f59da96b3912a3af3d8922e0b65d4eeed71c
Criswell-CC <53798287+Criswell-CC@users.noreply.github.com>
2019-08-06 14:49:17 -0500

    Add files via upload

---
+++ ViewModel/Commands/DelegateCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Input;
+using Roman.Models;
+
+namespace Roman.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Action _execute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action execute)
+                       : this(execute, null)
+        {
+        }
+
+        public DelegateCommand(Action execute,
+                       Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute())
+                return true;
+            else return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+            {
+                CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+
+}
