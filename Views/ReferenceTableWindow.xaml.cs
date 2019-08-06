e124dc400b5f76e9d03ff1dfed3c402a6a34fd34
Criswell-CC <53798287+Criswell-CC@users.noreply.github.com>
2019-08-06 14:45:00 -0500

    Add files via upload

---
+++ Views/ReferenceTableWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using Roman.Models;
+
+namespace Roman.Windows
+{
+    public partial class ReferenceTableWindow : Window
+    {
+        public ReferenceTableWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void ReferenceTableWindow_Closing(object sender, CancelEventArgs e)
+        {
+            ReferenceTableModel.windowOpen = false;
+        }
+    }
+}
