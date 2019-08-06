6d04b0c3db8b24e9e1cff40db1e93ed8680c92e8
Criswell-CC <53798287+Criswell-CC@users.noreply.github.com>
2019-08-06 14:40:59 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using Roman.ViewModels;
+
+namespace Roman
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new ConverterViewModel();
+            this.KeyDown += new KeyEventHandler(MainWindow_KeyDown);
+            CheckLogMenuItem.CommandTarget = this;
+        }
+
+        private void Button_OnClick(object sender, RoutedEventArgs e)
+        {
+                ConverterViewModel.DisplayResult();
+                Keyboard.Focus(InputBox);
+                InputBox.Text = "";
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                this.Button_OnClick(sender, e);
+            }
+        }
+
+        private void Close_Executed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
